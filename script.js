57b8e0869c8d57f1f0d76d478ebda6962f7134ac
sulyman002 <oyedelesulaiman@gmail.com>
2025-05-26 02:26:58 +0100

    new css feature added

---
+++ script.js
@@ -0,0 +1,4 @@
+const history_click = document.querySelector('.history_click');
+const vision_click = document.querySelector('.vision_click');
+const goals_click = document.querySelector('.goals_click');
+

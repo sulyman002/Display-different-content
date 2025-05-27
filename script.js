1fb700a90824ea5a6a16b5db7b5cafc588ed1eed
sulyman002 <oyedelesulaiman@gmail.com>
2025-05-27 01:53:01 +0100

    i run the javascript

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-const history_click = document.querySelector('.history_click');
-const vision_click = document.querySelector('.vision_click');
-const goals_click = document.querySelector('.goals_click');
 
+const historyBtn = document.getElementById('history_click');
+    const visionBtn = document.getElementById('vision_click');
+    const goalsBtn = document.getElementById('goal_click');
+
+    const historyCard = document.querySelector('.history_card');
+    const visionCard = document.querySelector('.vision_card');
+    const goalsCard = document.querySelector('.goals_card');
+
+    const historyContent = document.querySelector('.history_content');
+    const visionContent = document.querySelector('.vision-content');
+    const goalsContent = document.querySelector('.goals_content');
+
+    function hideAll() {
+        historyCard.style.display = 'none';
+        visionCard.style.display = 'none';
+        goalsCard.style.display = 'none';
+        historyContent.style.display = 'none';
+        visionContent.style.display = 'none';
+        goalsContent.style.display = 'none';
+    }
+
+    historyBtn.addEventListener('click', () => {
+        hideAll();
+        historyCard.style.display = 'block';
+        historyContent.style.display = 'flex';
+    });
+
+    visionBtn.addEventListener('click', () => {
+        hideAll();
+        visionCard.style.display = 'block';
+        visionContent.style.display = 'flex';
+    });
+
+    goalsBtn.addEventListener('click', () => {
+        hideAll();
+        goalsCard.style.display = 'block';
+        goalsContent.style.display = 'flex';
+    });
+
+    hideAll();
+    historyCard.style.display = 'block';
+    historyContent.style.display = 'flex';
+
+
+ 
